41bd151d2c3003a042384fa9bd079f57c718bb03
Garnett05 <charlleslimamota10@hotmail.com>
2020-02-01 13:15:13 -0300

    Introdução a Herança

---
+++ HerancaOO/Entities/BusinessAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HerancaOO.Entities
+{
+    class BusinessAccount : Account // BusinessAccount está herdando de Account todos os seus métodos e atributos
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        //neste dois pontos está sendo feita a reutilização do construtor da Account, utilizando o base e passando os argumentos em comum entre as classes
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }            
+        }
+    }
+}
---
+++ HerancaOO/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using HerancaOO.Entities;
+
+namespace HerancaOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Jarbas", 700.00, 300.00);
+            Console.WriteLine(account.Balance);            
+        }
+    }
+}
